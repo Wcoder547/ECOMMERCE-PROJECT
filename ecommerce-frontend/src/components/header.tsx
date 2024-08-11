21625acf187e7e44b22f55b1df8443261bd856eb
Wcoder547 <mw5667155@.com>
2024-08-11 15:15:02 +0500

    start the project and working on header ( 1 hr )

---
+++ ecommerce-frontend/src/components/header.tsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+
+const user = { _id: "65yh5", role: "admin" };
+
+const Header = () => {
+  const [isOpen, setisOpen] = useState<boolean>(false);
+  const handleSubmit = () => {
+    setisOpen(false);
+  };
+  return (
+    <nav className="header">
+      <Link onClick={() => setisOpen(false)} to={"/"}>
+        Home
+      </Link>
+      <Link onClick={() => setisOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setisOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+
+      {user?._id ? (
+        <>
+          <button onClick={() => setisOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <Link onClick={() => setisOpen(false)} to={"/admin/dashboard"}>
+                  Admin
+                </Link>
+              )}
+              <Link onClick={() => setisOpen(false)} to={"/orders"}>
+                Orders
+              </Link>
+              <button onClick={handleSubmit}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
